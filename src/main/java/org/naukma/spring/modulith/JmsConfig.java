9816868914702640833300400ba5acddbaab12ad
Daimonix <dimaprotsap@gmail.com>
2024-10-09 20:30:41 +0300

    [feature] Active MQ Artemis JMS integration

---
+++ src/main/java/org/naukma/spring/modulith/JmsConfig.java
@@ -0,0 +1,34 @@
+package org.naukma.spring.modulith;
+
+import jakarta.jms.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJms
+public class JmsConfig {
+    @Bean
+    public JmsListenerContainerFactory<?> jmsListenerFactory(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory,
+                                                             DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
--- src/main/java/org/naukma/spring/modulith/analytics/AnalyticsService.java
+++ src/main/java/org/naukma/spring/modulith/analytics/AnalyticsService.java
@@ -1,25 +1,15 @@
 package org.naukma.spring.modulith.analytics;
 
 import lombok.RequiredArgsConstructor;
-import org.naukma.spring.modulith.authentication.AuthenticationService;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class AnalyticsService {
-    private final WebClient webClient;
-    private final Duration timeout = Duration.ofSeconds(1);
-    private final AuthenticationService authenticationService;
+    private final JmsTemplate jmsTemplate;
 
     public void reportEvent(AnalyticsEventType event) {
-        webClient.post().uri("/analytics")
-                .header(authenticationService.getAuthHeader(), authenticationService.getAuthKey())
-                .bodyValue(new AnalyticsEvent(event))
-                .retrieve()
-                .bodyToMono(Void.class)
-                .block(timeout);
+        jmsTemplate.convertAndSend("analytics", event);
     }
 }
